62dd9a1519cf20c87ebac8fd6ee7e1afb619535b
Andre Bandarra <andreban@google.com>
2016-07-18 13:31:30 +0100

    Include Handlebars

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ const app = express();
 app.disable('etag');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 app.set('trust proxy', true);
 
 // Static files
