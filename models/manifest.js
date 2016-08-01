229dae9af5b905586f4acd02212780dc5fde7ba4
Andre Bandarra <andreban@google.com>
2016-08-01 13:28:14 +0100

    Move User-Agent to a static variable

--- models/manifest.js
+++ models/manifest.js
@@ -17,6 +17,8 @@
 
 const fetch = require('node-fetch');
 const DOMAIN_PATH_REGEXP = /(http[s]*:\/\/[a-z0-9A-Z-\.]+)(\/(.*?\/)*)*/;
+const USER_AGENT = `Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 
+  (KHTML, like Gecko) Chrome/48.0.2564.23 Mobile Safari/537.36`;
 
 class Manifest {
   getBestIcon() {
@@ -80,10 +82,7 @@ class Manifest {
     const options = {
       method: 'GET',
       headers: {
-        'user-agent': [
-          'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36',
-          '(KHTML, like Gecko) Chrome/48.0.2564.23 Mobile Safari/537.36'
-        ].join(' ')
+        'user-agent': USER_AGENT
       }
     };
 
