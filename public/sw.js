c0d59b45d268716eefdca5b48c905c1e44873ac1
Michael Stillwell <mjs@beebo.org>
2016-09-05 16:25:42 +0100

    Disable unvailable PWAs from list view

--- public/sw.js
+++ public/sw.js
@@ -14,6 +14,28 @@ const OFFLINE_URL = '/.shell/offline';
 toolbox.precache(OFFLINE);
 
 toolbox.options.debug = true;
+
+// Provide an API for the front end to determine which resources are available in the
+// cache. We could have also established a new endpoint, or implemented support
+// for 'only-if-cached', but the HEAD method was unused.
+toolbox.router.head(/^/, request => {
+  // Simulate RFC7234's only-if-cached mechanism so that the front end is able to
+  // determine which links work offline. Also very similar to sw-toolbox's cacheOnly
+  // strategy, except that we cache.match(url) rather than cache.match(request) so
+  // that the HEAD method is ignored on cache.match().
+  function onlyIfCached(url) {
+    return caches.open(toolbox.options.cache.name).then(cache => {
+      return cache.match(url).then(response => {
+        return (response && response.status === 200) ? response : new Response('', {
+          status: 504,
+          statusText: 'Gateway Timeout'
+        });
+      });
+    });
+  }
+  return onlyIfCached(request.url);
+});
+
 toolbox.router.default = (request, values, options) => {
   return toolbox.networkFirst(request, values, options)
     .catch(_ => {
