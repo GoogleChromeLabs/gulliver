48c2ed273e43c29a3135e057bfef9e2ccfabaa45
Alberto Medina <albertomedina@google.com>
2016-09-22 13:38:11 -0400

    Add template files for detail views. Add comment to highlighting function.

--- views/helpers/index.js
+++ views/helpers/index.js
@@ -44,6 +44,10 @@ exports.moment = function(date) {
   return moment(date).fromNow();
 };
 
+/* 
+  Light-weight helper function to generate
+  highlighted JSON strings
+*/
 function syntaxHighlight(json) {
     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
