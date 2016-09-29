b1758e271683dd1e43619046647f17407c0b37aa
Michael Stillwell <mjs@beebo.org>
2016-09-29 14:56:24 +0100

    Fix offline state edge case

--- public/sw.js
+++ public/sw.js
@@ -65,3 +65,10 @@ toolbox.router.default = (request, values, options) => {
       });
     });
 };
+
+// Claim clients so that the very first page load is controlled by a service
+// worker. (Important for responding correctly in offline state.)
+self.addEventListener('activate', () => self.clients.claim());
+
+// Make sure the SW the page we register() is the service we use.
+self.addEventListener('install', () => self.skipWaiting());
