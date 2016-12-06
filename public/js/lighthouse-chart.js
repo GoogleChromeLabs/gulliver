286a7facf11ef8759496194af5c1de0b64eb1e9d
Sebastian Benz <sbenz@google.com>
2016-12-06 17:14:41 +0000

    refactor pwa detail view
    PWA details card:
    
    * remove unneeded divs
    * make the whole card a link (instead of declaring three different
    links)
    * add card hover effect (WDYT?)
    * add card touch effect
    * small design changes (more consistent font sizes)
    
    Lighthouse card:
    
    * extract score into separate partial template
    * fade in chart
    * reserve chart space with border
    * show spinner while chart is loading

--- public/js/lighthouse-chart.js
+++ public/js/lighthouse-chart.js
@@ -1,24 +1,51 @@
+/**
+ * copyright 2015-2016, google, inc.
+ * licensed under the apache license, version 2.0 (the "license");
+ * you may not use this file except in compliance with the license.
+ * you may obtain a copy of the license at
+ *
+ *    http://www.apache.org/licenses/license-2.0
+ *
+ * unless required by applicable law or agreed to in writing, software
+ * distributed under the license is distributed on an "as is" basis,
+ * without warranties or conditions of any kind, either express or implied.
+ * see the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+
 /* global google */
 /* eslint-env browser */
 
 /**
  * Use to make the API request to get the Lighthouse chart data for a PWA.
  */
-const CHART_ELEMENT_ID = 'chart';
 const CHART_BASE_URL = '/api/lighthouse/graph/';
 
-function drawChart() {
-  const pagewith = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-  const chartDiv = document.getElementById(CHART_ELEMENT_ID);
-  const pwaId = chartDiv.getAttribute('pwa');
-  if (pwaId) {
+class LighthouseChart {
+
+  constructor() {
+    this.chartElement = document.getElementById('chart');
+    this.loader = new window.Loader(this.chartElement);
+  }
+
+  load() {
+    this.loader.show();
+    google.charts.load('current', {packages: ['annotationchart']});
+    google.charts.setOnLoadCallback(this.drawChart.bind(this));
+  }
+
+  drawChart() {
+    const pwaId = this.chartElement.getAttribute('pwa');
+    if (!pwaId) {
+      return;
+    }
+    const pagewith = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     fetch(CHART_BASE_URL + pwaId)
       .then(response => response.json())
       .then(jsonData => {
         // Create our data table out of JSON data loaded from server.
         const data = new google.visualization.DataTable(jsonData);
-        const chart = new google.visualization.AnnotationChart(
-          document.getElementById(CHART_ELEMENT_ID));
+        const chart = new google.visualization.AnnotationChart(this.chartElement);
         const options = {
           height: 242,
           displayAnnotations: false,
@@ -30,14 +57,13 @@ function drawChart() {
           max: 100
         };
         chart.draw(data, options);
+        this.loader.hide();
       })
       .catch(err => {
         console.error('There was an error drawing the chart!', err);
       });
   }
+
 }
 
-if (document.getElementById(CHART_ELEMENT_ID)) {
-  google.charts.load('current', {packages: ['annotationchart']});
-  google.charts.setOnLoadCallback(drawChart);
-}
+new LighthouseChart().load();
