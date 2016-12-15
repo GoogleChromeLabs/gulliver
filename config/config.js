60f0ffa823d2e42ca6c10994d1f5ed8f9b538e7e
Michael Stillwell <mjs@beebo.org>
2016-12-15 12:05:44 +0000

    Improve config error detection handling (#272)
    * Improve config error detection handling
    * Improve "help" strings
    * Remove DATA_BACKEND
    * Upgrade warning to error
    * Set env vars on Travis

--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@
 
 // Hierarchical node.js configuration with command-line arguments, environment
 // variables, and files.
-const nconf = module.exports = require('nconf');
+const nconf = require('nconf');
 const path = require('path');
 
 nconf
@@ -26,7 +26,6 @@ nconf
   // 2. Environment variables
   .env([
     'CLOUD_BUCKET',
-    'DATA_BACKEND',
     'GCLOUD_PROJECT',
     'PORT',
     'CLIENT_ID',
@@ -41,15 +40,7 @@ nconf
   .file({file: path.join(__dirname, 'config.json')})
   // 4. Defaults
   .defaults({
-    // Typically you will create a bucket with the same name as your project ID.
-    CLOUD_BUCKET: '',
-    DATA_BACKEND: 'datastore',
-
-    // This is the id of your project in the Google Cloud Developers Console.
-    GCLOUD_PROJECT: '',
-
-    // Port the HTTP server
-    PORT: 8080
+    PORT: 8080 // Port used by HTTP server
   });
 
 // Check for required settings
@@ -59,8 +50,14 @@ checkConfig('CLIENT_ID');
 checkConfig('CLIENT_SECRET');
 
 function checkConfig(setting) {
+  // If setting undefined, throw error
   if (!nconf.get(setting)) {
-    throw new Error('You must set the ' + setting + ' environment variable or' +
-      ' add it to config.json!');
+    throw new Error(`You must set the ${setting} environment variable or add it to config.json!`);
+  }
+  // If setting includes a space, throw error
+  if (nconf.get(setting).match(/\s/)) {
+    throw new Error(`The ${setting} environment variable is suspicious ("${nconf.get(setting)}")`);
   }
 }
+
+module.exports = nconf;
