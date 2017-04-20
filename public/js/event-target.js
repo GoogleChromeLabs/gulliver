1bd7e36eb57091ee61cc37ee495c01b5c4e8d942
Andre Bandarra <andreban@google.com>
2017-04-20 15:00:55 +0100

    Fix nit on event-targetr

--- public/js/event-target.js
+++ public/js/event-target.js
@@ -45,8 +45,6 @@ export default class EventTarget {
       return;
     }
 
-    typeListeners.forEach(callback => {
-      callback(event);
-    });
+    typeListeners.forEach(callback => callback(event));
   }
 }
