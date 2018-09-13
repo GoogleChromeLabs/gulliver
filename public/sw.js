a199d94513526da98071518a120454af49b8fc9f
Julian Toledo <juliantoledo@gmail.com>
2018-09-13 09:22:03 +0200

    Increase sw version

--- public/sw.js
+++ public/sw.js
@@ -11,7 +11,7 @@ toolbox.options.debug = false;
 
 importScripts('/js/sw-assets-precache.js'); /* global ASSETS */
 
-const VERSION = '19';
+const VERSION = '20';
 const PREFIX = 'gulliver';
 const CACHE_NAME = `${PREFIX}-v${VERSION}`;
 const PWA_OPTION = {
